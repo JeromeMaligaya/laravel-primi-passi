989e4d948a8f1e56d79fdfc829997de5ae0f0b4b
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-21 23:15:36 +0200

    added Route for home.blade.php in web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    $title= "Hello World";
+
+    return view('home', compact("title"));
 });
